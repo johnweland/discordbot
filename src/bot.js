dd3bc075bcfb4c5f60f058e018e0e35f737fe682
John Weland <jweland@icsolutions.com>
2020-08-26 13:37:57 -0500

    added a basic command handler

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
+const config = require('./config.json');
+const command = require('./command');
 
 const { Client } = require('discord.js');
 
 const client = new Client();
+client.on('ready', () => {
+    console.log(`${client.user.tag} is ${client.user.presence.status} in ${client.guilds.cache.size} server.`);
 
+    command(client, 'ping', message => {
+        message.channel.send("Pong!");
+    })
+});
 client.login(process.env.DISCORD_BOT_TOKEN);
